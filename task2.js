a0509b0b1e2306d1e5f638b809a09982a3daa768
Aman Kumar <akvir10157@gmail.com>
2020-08-17 12:33:53 +0530

    first commit

---
+++ task2.js
@@ -0,0 +1,10 @@
+(function (){
+    var script= document.getElementsByTagName('p')
+    
+    for (let i=0; i<script.length; i++){
+        script[i].style['background-color']='red';
+    }
+
+    
+
+})();
